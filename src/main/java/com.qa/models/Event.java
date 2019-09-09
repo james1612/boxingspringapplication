45b6ac5aed1d99a967f36909db9c662afff084cc
james1612 <james.irish18@gmail.comgit add *.javagit config --global user.email james.irish18@gmail.com>
2019-09-09 16:00:15 +0100

    added Event and Event controller files

---
+++ src/main/java/com.qa/models/Event.java
@@ -0,0 +1,59 @@
+package com.qa.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String boxer1Name;
+    private String boxer2Name;
+    private String location;
+    private String result;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBoxer1Name() {
+        return boxer1Name;
+    }
+
+    public void setBoxer1Name(String boxer1Name) {
+        this.boxer1Name = boxer1Name;
+    }
+
+    public String getBoxer2Name() {
+        return boxer2Name;
+    }
+
+    public void setBoxer2Name(String boxer2Name) {
+        this.boxer2Name = boxer2Name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+}
